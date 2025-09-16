c37eab7ce7f10f33350f0bd9f7030011234ef6e2
raccoon0513 <axm9993@gmail.com>
2025-09-16 09:46:41 +0900

    somthing wrong

--- splascii/src/App.js
+++ splascii/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
 function App() {
+   const board = Array(64).fill(null);
+
+  const renderBoard = board.map((_, index) => {
+    return(
+      <div className={index}>
+        <p>test</p>
+      </div>
+    )
+  });
   return (
     <div className="App">
-      
+      {renderBoard}
     </div>
   );
+  
 }
 
 export default App;
